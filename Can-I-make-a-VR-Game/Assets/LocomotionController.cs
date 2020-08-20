387bc2c7cbdf7002ec6d09f1cd90b7a0fbb92734
Curti-Windows\Curti <joaocurti@hotmail.com>
2020-08-20 15:08:44 +0200

    Finished Teleport System
    Button A of right controller is the trigger

---
+++ Can-I-make-a-VR-Game/Assets/LocomotionController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class LocomotionController : MonoBehaviour
+{
+    public XRController rightTeleportRay;
+    private XRRayInteractor rightRayInteractor;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rightTeleportRay);
+        {
+            rightRayInteractor = rightTeleportRay.gameObject.GetComponent<XRRayInteractor>();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightTeleportRay)
+        {
+            rightRayInteractor.allowSelect = checkIfActivated(rightTeleportRay);
+            rightTeleportRay.gameObject.SetActive(checkIfActivated(rightTeleportRay));
+        }
+        
+    }
+    public bool checkIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+
+}

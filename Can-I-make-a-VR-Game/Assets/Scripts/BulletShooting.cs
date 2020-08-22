ead0abd6dbbdc659d3b5cce7103166f51a518ae9
Curti-Windows\Curti <joaocurti@hotmail.com>
2020-08-22 13:35:18 +0200

    Added example of gun fire script with sound and continuous rigid body collision

---
+++ Can-I-make-a-VR-Game/Assets/Scripts/BulletShooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShooting : MonoBehaviour
+{
+    public float speed = 40;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+       
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    public void Fire()
+    {
+        Quaternion bulletRotation = Quaternion.Euler(barrel.position.x+90, barrel.position.y, barrel.position.z);
+        GameObject spawnedBullet = Instantiate(bullet, barrel.position, bulletRotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        audioSource.PlayOneShot(audioClip);
+        Destroy(spawnedBullet, 5);
+    }
+
+}

85e9db75822697f76db999b8353c1a33cf9d1d4a
Curti-Windows\Curti <joaocurti@hotmail.com>
2020-08-30 19:39:30 +0200

    added blender sword interaction script to vibrate sword when hovered

---
+++ Can-I-make-a-VR-Game/Assets/Swordbehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Swordbehaviour : XRGrabInteractable
+{
+    public Transform objectToVibrate;
+
+
+    private Vector3 initialPosition;
+
+    public float amplitude; // the amount it moves
+    public float frequency; // the period of the earthquake
+    public Material normalMaterial;
+    public Material hoveredMaterial;
+    public MeshRenderer swordObjectRenderer;
+
+    void Start()
+    {
+        initialPosition = objectToVibrate.position; // store this to avoid floating point error drift
+}
+
+
+
+    void Update()
+    {
+        initialPosition = objectToVibrate.position;
+        if (isHovered && !isSelected)
+        { 
+        objectToVibrate.position = initialPosition + (objectToVibrate.forward * Mathf.Sin(frequency * Time.time) * amplitude) + objectToVibrate.right * Mathf.Sin(frequency/2 * Time.time) * amplitude + objectToVibrate.up * Mathf.Sin(frequency /3  * Time.time) * amplitude;
+        }
+        if (isSelected && swordObjectRenderer.material != normalMaterial)
+        {
+            swordObjectRenderer.material = normalMaterial;
+        }
+    }
+
+    protected override void OnHoverEnter(XRBaseInteractor interactor)
+    {
+        base.OnHoverEnter(interactor);
+        swordObjectRenderer.material = hoveredMaterial;
+        
+
+
+
+    }
+    protected override void OnHoverExit(XRBaseInteractor interactor)
+    {
+        base.OnHoverExit(interactor);
+        swordObjectRenderer.material = normalMaterial;
+
+    }
+     
+
+
+
+
+}
